b943f2efe44312a3bb0e6c9aed1090c2310cc0c6
ASHU <160227183+asharibshahid@users.noreply.github.com>
2024-09-23 12:12:45 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,18 @@
+
+import react from 'react';
+import Header from "./components/header"; 
+import AboutUs from './components/About';
+
+
+const HomePage=() => {
+return(
+
+<div>
+  <h1>Home</h1>
+ <AboutUs  />
+ <Header  />
+
+</div>
+)
+}
+export default HomePage;
